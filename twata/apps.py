d153d164cb14467b00b3ab050299ad236a473c8f
lamar133 <lamar_johnson133@yahoo.ca>
2017-11-28 23:59:12 -0500

    initial file configuration

---
+++ twata/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TwataConfig(AppConfig):
+    name = 'twata'
---
+++ twata_dj/views.py
@@ -0,0 +1,6 @@
+# twata_dj/views.py
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
